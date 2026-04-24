279f6ada64262042f97d4eb729355523e206821b
aynainmustafa-wq <aynainmustafa@gmail.com>
2026-04-24 11:22:05 +0530

    Add files via upload

---
+++ arr_use_marks.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int marks[4] = {9, 8, 6, 5};
+
+    for (int i = 0; i <= 3; i++) {
+        printf("Value of array at index %d is %d \n", i, marks[i]);
+    }
+
+    return 0;
+}
+     
