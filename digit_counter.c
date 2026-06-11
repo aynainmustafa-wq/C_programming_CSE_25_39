64a82ac7027e90178f2bdb25ac1181ce87f28e2e
aynainmustafa-wq <aynainmustafa@gmail.com>
2026-06-11 15:10:08 +0530

    digit_counter.c

---
+++ digit_counter.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+    int num;
+    int count = 0;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    if (num == 0) count = 1;
+    while (num != 0) {
+        num /= 10;
+        count++;
+    }
+    printf("Total digits: %d", count);
+    return 0;
+}
